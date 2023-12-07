aeed8a7a10bddc1a64e7dae8a7fac377ca834c46
sarmaier <sarmaier@iu.edu>
2023-12-07 11:47:10 -0500

    error handling main

--- generate_descriptors/generate_fingerprints.py
+++ generate_descriptors/generate_fingerprints.py
@@ -27,34 +27,38 @@ def get_unique_smiles(_smiles):
     return unique
 
 
-# load fragment smiles and connectivity information
-smiles = load_json("fragment_smiles.json")
-conn_dist = load_json("bond_lengths_away.json")
+if __name__ == "__main__":
+    # load fragment smiles and connectivity information
+    try:
+        smiles = load_json("fragment_smiles.json")
+        conn_dist = load_json("bond_lengths_away.json")
+    except FileNotFoundError:
+        print("Sorry, fragment or bond info missing. Failed to generate fingerprints.")
+        exit()
+    # consider only those molecules in the train + val (not test) set
+    train_index = list(pd.read_csv("train_indices.csv")['file'])
+    all_smiles = [list(smiles[x].values()) for x in smiles if int(x) in train_index]
 
-# consider only those molecules in the train + val (not test) set
-train_index = list(pd.read_csv("train_indices.csv")['file'])
-all_smiles = [list(smiles[x].values()) for x in smiles if int(x) in train_index]
+    # get list of unique smiles from dataset considered
+    unique_smiles = get_unique_smiles(all_smiles)
+    all_features = {}
+    for value in smiles:
+        count = {x: list(smiles[value].values()).count(x) for x in unique_smiles}
+        features = []
+        for i in unique_smiles:
+            tops = [conn_dist[value][x] for x in conn_dist[value] if smiles[value][x] == i]
+            if len(tops) > 0:
+                max_tops = max(tops)
+                features.extend([max_tops])
+            else:
+                features.extend([0])
+        features = np.array(features)
+        all_features[int(value)] = features
 
-# get list of unique smiles from dataset considered
-unique_smiles = get_unique_smiles(all_smiles)
-all_features = {}
-for value in smiles:
-    count = {x: list(smiles[value].values()).count(x) for x in unique_smiles}
-    features = []
-    for i in unique_smiles:
-        tops = [conn_dist[value][x] for x in conn_dist[value] if smiles[value][x] == i]
-        if len(tops) > 0:
-            max_tops = max(tops)
-            features.extend([max_tops])
-        else:
-            features.extend([0])
-    features = np.array(features)
-    all_features[int(value)] = features
+    feature_value = pd.DataFrame(unique_smiles)
+    df_features = pd.DataFrame(all_features).T.sort_index()
+    df_features = df_features.reset_index()
 
-feature_value = pd.DataFrame(unique_smiles)
-df_features = pd.DataFrame(all_features).T.sort_index()
-df_features = df_features.reset_index()
-
-# create csv of CBH features and feature identities (ordering will change)
-feature_value.to_csv("cbh_feature_identity.csv")
-df_features[:2389].to_csv("cbh_features_" + str(len(df_features)) + ".csv")
+    # create csv of CBH features and feature identities (ordering will change)
+    feature_value.to_csv("cbh_feature_identity.csv")
+    df_features[:2389].to_csv("cbh_features_" + str(len(df_features)) + ".csv")
